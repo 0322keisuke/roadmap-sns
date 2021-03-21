7b6ab2212f998166cf46f32e6d4644d5a0ff4a34
github 0322keisuke <isikeidesk@gmail.com>
2021-03-21 23:57:03 +0900

    詳細画面ルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\RoadmapController;
 Auth::routes();
 Route::get('/login/guest', 'Auth\LoginController@authenticate');
 Route::get('/', 'RoadmapController@index')->name('roadmaps.index');
+Route::get('roadmaps/{roadmap}', 'RoadmapController@show')->name('roadmaps.show');
 
 Route::get('roadmaps/create', 'RoadmapController@create')->name('roadmaps.create')->middleware('auth');
 Route::post('roadmaps/store', 'RoadmapController@store')->name('roadmaps.store')->middleware('auth');
