d54079be4fcbf8f3b73521e2a9bfd4918740884b
github 0322keisuke <isikeidesk@gmail.com>
2021-03-22 14:14:50 +0900

    showメソッドの作成+create画面が表示されない不具合の修正

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,12 @@ use App\Http\Controllers\RoadmapController;
 Auth::routes();
 Route::get('/login/guest', 'Auth\LoginController@authenticate');
 Route::get('/', 'RoadmapController@index')->name('roadmaps.index');
-Route::get('roadmaps/{roadmap}', 'RoadmapController@show')->name('roadmaps.show');
 
-Route::get('roadmaps/create', 'RoadmapController@create')->name('roadmaps.create')->middleware('auth');
-Route::post('roadmaps/store', 'RoadmapController@store')->name('roadmaps.store')->middleware('auth');
 
+Route::get('/roadmaps/create', 'RoadmapController@create')->name('roadmaps.create')->middleware('auth');
+Route::post('/roadmaps/store', 'RoadmapController@store')->name('roadmaps.store')->middleware('auth');
+
+Route::get('/roadmaps/{roadmap}', 'RoadmapController@show')->name('roadmaps.show');
 
 Route::get('/tutorials', 'TutorialController@index')->name('tutorials.index')->middleware('auth');
 Route::post('/tutorials/store', 'TutorialController@store')->middleware('auth');
