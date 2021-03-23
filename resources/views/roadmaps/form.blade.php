3fe554f405a9d7ff24b7cf6c8b63831a7b75c826
github 0322keisuke <isikeidesk@gmail.com>
2021-03-23 14:05:36 +0900

    教材名以外、DBの値を取得

--- resources/views/roadmaps/form.blade.php
+++ resources/views/roadmaps/form.blade.php
@@ -1,7 +1,7 @@
 @csrf
 <div class="md-form">
   <label for="title">ロードマップ名</label>
-  <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}">
+  <input type="text" name="title" id="title" class="form-control" value="{{ $roadmap->title ?? old('title') }}">
 </div>
 
 <label> 教材名/タスク名 </label>
@@ -9,17 +9,17 @@
 
 <div class="form-group">
   <label></label>
-  <textarea name="body" class="form-control" rows="8" placeholder="説明">{{ old('body') }}</textarea>
+  <textarea name="body" class="form-control" rows="8" placeholder="説明">{{ $roadmap->body ?? old('body') }}</textarea>
 </div>
 
 <div class="md-form">
   <label for="estimated_time">学習時間目安(単位：時間)</label>
-  <input type="number" name="estimated_time" id="estimated_time" class="form-control" value="{{ old('estimated_time') }}" min="1" max="300">
+  <input type="number" name="estimated_time" id="estimated_time" class="form-control" value="{{ $roadmap->estimated_time ?? old('estimated_time') }}" min="1" max="300">
 </div>
 
 <div class="form-group">
   <label>学習レベル</label>
-  <select name="level" class="form-control" value="{{ old('level') }}">
+  <select name="level" class="form-control" value="{{ $roadmap->level ?? old('level') }}">
     <option value="1">初級</option>
     <option value="2">中級</option>
     <option value="3">上級</option>
