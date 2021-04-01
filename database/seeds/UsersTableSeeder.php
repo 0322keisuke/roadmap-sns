559ae1d3a4ffe53e74749aa958a906680fe2d349
github 0322keisuke <isikeidesk@gmail.com>
2021-04-01 13:33:35 +0900

    プロフィールページの表示

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,5 +20,21 @@ class UsersTableSeeder extends Seeder
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
+
+        DB::table('users')->insert([
+            'name' => 'guest2',
+            'email' => 'guest2@guest.com',
+            'password' => bcrypt('guestpass2'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'guest3',
+            'email' => 'guest3@guest.com',
+            'password' => bcrypt('guestpass3'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
