e219d23ef009c7a93dd68a7a5890fd670775b472
github 0322keisuke <isikeidesk@gmail.com>
2021-04-02 18:38:35 +0900

    プロフィールのタブBladeの共通化

--- resources/views/users/likes.blade.php
+++ resources/views/users/likes.blade.php
@@ -6,18 +6,7 @@
 @include('nav')
 <div class="container">
   @include('users.user')
-  <ul class="nav nav-tabs nav-justified mt-3">
-    <li class="nav-item">
-      <a class="nav-link text-muted" href="{{ route('users.show', ['name' => $user->name]) }}">
-        ロードマップ
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link text-muted active" href="{{ route('users.likes', ['name' => $user->name]) }}">
-        いいね
-      </a>
-    </li>
-  </ul>
+  @include('users.tabs',['hasRoadmaps' => false,'hasLikes' => true])
   @foreach($roadmaps as $roadmap)
   @include('roadmaps.card')
   @endforeach
