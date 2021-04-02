c85fc61c1b1327f0f8e0ca76b8b7f19958dced77
github 0322keisuke <isikeidesk@gmail.com>
2021-04-02 16:27:11 +0900

    index.blade.phpをcard部分と分割+リンク範囲修正

---
+++ resources/views/roadmaps/card.blade.php
@@ -0,0 +1,37 @@
+<div class="col-md-6 mb-3">
+  <div class="card h-100">
+    <a href="{{ route('users.show', ['name' => $roadmap->user->name]) }}" class="text-dark">
+      <div class="card-header d-flex flex-row">
+        <i class="fas fa-user-circle fa-3x mr-1"></i>
+        <div>
+          <div class="font-weight-bold">
+            {{ $roadmap->user->name }}
+          </div>
+          <div class="font-weight-lighter">
+            {{ $roadmap->created_at->format('Y/m/d H:i') }}
+          </div>
+        </div>
+      </div>
+    </a>
+    <div class="card-body pt-0 pb-2">
+      <a class="text-dark" href="{{route('roadmaps.show',['roadmap' => $roadmap])}}">
+        <h3 class="h4 card-title pt-2">
+          {{ $roadmap->title }}
+        </h3>
+        <div class="card-text">
+          <div class="estimated-time mr-1">
+            <i class="far fa-clock"></i>
+            <span class="mr-3">{{ $roadmap->estimated_time }}h</span>
+            <span>Lv {{ $roadmap->level_text }}</span>
+          </div>
+        </div>
+    </div>
+    </a>
+    <div class="card-body pt-0 pb-2 pl-3">
+      <div class="card-text">
+        <roadmap-like :initial-is-liked-by='@json($roadmap->isLikedBy(Auth::user()))' :initial-count-likes='@json($roadmap->count_likes)' :authorized='@json(Auth::check())' endpoint="{{ route('roadmaps.like',['roadmap' => $roadmap]) }}">
+        </roadmap-like>
+      </div>
+    </div>
+  </div>
+</div>
