0288a4554e3caa1a9e28b0b77cea5404f7a0d23d
github 0322keisuke <isikeidesk@gmail.com>
2021-04-17 20:40:57 +0900

    TaskUpdateのテスト

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -25,4 +25,21 @@ class TaskControllerTest extends TestCase
 
     $response->assertStatus(200);
   }
+
+  public function testTaskUpdate(){
+    $user = factory(User::class)->create();
+    $tutorial = factory(Tutorial::class)->create(['user_id' => $user->id]);
+    $task = factory(Task::class)->create(['tutorial_id'=> $tutorial->id]);
+
+    $response = $this->actingAs($user)->patch(route('tasks.update'), [ 
+      'displayTutorialId' => 1,
+      'id' => 35,
+      'newIndex' => 0,
+      'oldIndex' => 2,
+      'status' => 2,
+      'tasks' => ['status'=>1,['tasks' => ['created_at' => '2021-04-16 17:33:50','id' => 35,'name' => "6",'order' => 2,'status'=>1,'tutorial_id'=>1,'updated_at'=> '2021-04-16 17:33:50'],'title'=>"Todo",'tutorial_id'=>1]]
+    ]);
+
+    $response->assertStatus(200);
+  }
 }
