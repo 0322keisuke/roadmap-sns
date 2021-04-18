1e99e08e8dacd1dff1f41f7ff6cc58457f85eb54
github 0322keisuke <isikeidesk@gmail.com>
2021-04-18 23:16:08 +0900

    Tasks.destroyテストの追加

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\User;
 use App\Tutorial;
+use App\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -39,6 +40,16 @@ class TaskControllerTest extends TestCase
       'status' => 2,
       'tasks' => ['status'=>1,['tasks' => ['created_at' => '2021-04-16 17:33:50','id' => 35,'name' => "6",'order' => 2,'status'=>1,'tutorial_id'=>1,'updated_at'=> '2021-04-16 17:33:50'],'title'=>"Todo",'tutorial_id'=>1]]
     ]);
+
+    $response->assertStatus(200);
+  }
+
+  public function testTaskDestroy(){
+    $user = factory(User::class)->create();
+    $tutorial = factory(Tutorial::class)->create(['user_id' => $user->id]);
+    $task = factory(Task::class)->create(['tutorial_id'=> $tutorial->id]);
+
+    $response = $this->actingAs($user)->delete(route('tasks.destroy',['task' => $task->id]));
 
     $response->assertStatus(200);
   }
