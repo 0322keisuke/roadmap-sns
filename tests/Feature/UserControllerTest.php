e8a30cda282af9c941753a7a6a2fbf1e5756107d
github 0322keisuke <isikeidesk@gmail.com>
2021-04-19 20:52:47 +0900

    users.showテストの作成

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use App\Roadmap;
+use App\Tutorial;
+use App\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserShow()
+    {
+        $user = factory(User::class)->create();
+        $roadmap = factory(Roadmap::class)->create(['user_id' => $user->id]);
+        $tutorial = factory(Tutorial::class)->create(['user_id' => $user->id]);
+        $task = factory(Task::class)->create(['tutorial_id' => $tutorial->id]);
+
+        $response = $this->actingAs($user)->get(route('users.show', ['name' => $user->name]));
+
+        $response->assertStatus(200);
+    }
+}
